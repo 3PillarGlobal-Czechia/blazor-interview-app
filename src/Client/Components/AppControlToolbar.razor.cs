60554d7c8b15b3c7554cd05fe65dcf6975e77a2d
Robert Žerdík <thezerda@users.noreply.github.com>
2021-09-03 03:42:00 +0200

    Componentize header, search and toolbar

---
+++ src/Client/Components/AppControlToolbar.razor.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.AspNetCore.Components;
+
+namespace InterviewApp.Client.Components;
+
+public partial class AppControlToolbar
+{
+    [Parameter]
+    public EventCallback<string> OnFilterList { get; set; }
+
+    [Parameter]
+    public EventCallback<object> OnOpenResetAllDialog { get; set; }
+
+    public string? SearchValue { get; set; }
+
+    public async Task FilterList(string? search)
+    {
+        await OnFilterList.InvokeAsync(search);
+    }
+
+    public async Task OpenResetAllDialog()
+    {
+        await OnOpenResetAllDialog.InvokeAsync();
+    }
+}
---
+++ src/Client/Components/AppHeader.razor.cs
@@ -0,0 +1,10 @@
+﻿
+using Microsoft.AspNetCore.Components;
+
+namespace InterviewApp.Client.Components;
+
+public partial class AppHeader
+{
+    [Parameter]
+    public string? Title { get; set; }
+}
