4a07fc627e4891ad1b72a30b048f7e05cca33ce4
Robert Žerdík <thezerda@users.noreply.github.com>
2021-09-06 15:26:33 +0200

    Compontentize expand list, pin toggle, add random extensions (not literally)

---
+++ src/Client/Shared/AppExpansionList.razor.cs
@@ -0,0 +1,37 @@
+﻿
+using InterviewApp.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace InterviewApp.Client.Shared;
+
+public partial class AppExpansionList
+{
+    [Parameter]
+    public List<InterviewQuestion>? InterviewQuestions { get; set; }
+
+    [Parameter]
+    public EventCallback<InterviewQuestion> OnTogglePin { get; set; }
+
+    [Parameter]
+    public EventCallback<InterviewQuestion> OnOpenNoteDialog { get; set; }
+
+    [Parameter]
+    public EventCallback<InterviewQuestion> OnOpenResetDialog { get; set; }
+
+    public async Task TogglePin(InterviewQuestion question)
+    {
+        await OnTogglePin.InvokeAsync(question);
+    }
+
+    public async Task OpenNoteDialog(InterviewQuestion question)
+    {
+        await OnOpenNoteDialog.InvokeAsync(question);
+    }
+
+    public async Task OpenResetDialog(InterviewQuestion question)
+    {
+        await OnOpenResetDialog.InvokeAsync(question);
+    }
+}
---
+++ src/Client/Shared/AppPinToggle.razor.cs
@@ -0,0 +1,19 @@
+﻿
+using InterviewApp.Shared.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace InterviewApp.Client.Shared;
+
+public partial class AppPinToggle
+{
+    [Parameter]
+    public InterviewQuestion? Question { get; set; }
+
+    [Parameter]
+    public EventCallback<InterviewQuestion> OnToggledChanged { get; set; }
+
+    public async Task ToggledChanged(InterviewQuestion question)
+    {
+        await OnToggledChanged.InvokeAsync(question);
+    }
+}
