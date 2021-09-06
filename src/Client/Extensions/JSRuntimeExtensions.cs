d8ce5564dd31114fdc210a840e4a0b3d2eda3dc1
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2021-09-06 21:07:56 +0200

    Introduce expression body method
    Introduce expression body methon on SaveAsAsync method in JSRuntimeExntensions class

--- src/Client/Extensions/JSRuntimeExtensions.cs
+++ src/Client/Extensions/JSRuntimeExtensions.cs
@@ -5,8 +5,6 @@ namespace InterviewApp.Client.Extensions;
 
 public static class JSRuntimeExtensions
 {
-    public async static Task SaveAsAsync(this IJSRuntime js, string fileName, byte[] data)
-    {
-        await js.InvokeAsync<object>("saveAsFile", fileName, Convert.ToBase64String(data));
-    }
+    public async static Task SaveAsAsync(this IJSRuntime js, string fileName, byte[] data) 
+        => await js.InvokeAsync<object>("saveAsFile", fileName, Convert.ToBase64String(data));
 }
